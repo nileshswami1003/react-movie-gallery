fd775380e62f8295e03d808f65812247aad5b500
nileshswami1003 <s.nilesh1003@gmail.com>
2024-01-29 19:09:16 +0530

    first commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,29 @@
+function ListGroup() {
+  let items = [
+    "India",
+    "Mumbai",
+    "Delhi",
+    "Banglore",
+    "Calcutta",
+    "Chennai",
+    "Karnataka",
+    "Goa",
+  ];
+
+  items = [];
+
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
